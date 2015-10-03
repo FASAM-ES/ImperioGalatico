fcfdb963f85ccea7f7b2f25a6ae778dd75001754
alysson10 <alysson10@gmail.com>
2015-10-03 11:01:19 -0300

    Criando classe Hello World!

---
+++ ProjetoIntegracao/src/main/java/br/edu/fasam/projetointegracao/FigurasApp.java
@@ -0,0 +1,12 @@
+
+package br.edu.fasam.projetointegracao;
+
+/**
+ *
+ * @author Alysson
+ */
+public class FigurasApp {
+    public static void main(Strings[] args){
+        System.out.println("Olá mundo!");
+    }
+}
