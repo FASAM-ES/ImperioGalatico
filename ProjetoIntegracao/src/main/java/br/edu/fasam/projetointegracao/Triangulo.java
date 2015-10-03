87d1b1e067ada4984a306069c9a5a565b7b96c44
Rogério <Rogrio@172.16.6.251>
2015-10-03 13:51:49 -0300

    Calculo Trinagulo

--- ProjetoIntegracao/src/main/java/br/edu/fasam/projetointegracao/Triangulo.java
+++ ProjetoIntegracao/src/main/java/br/edu/fasam/projetointegracao/Triangulo.java
@@ -6,8 +6,36 @@ public class Triangulo {
 
         private float lado;
         private float area;
-        private float altura;
         private float perimetro;
+        private Integer lado1;
+        private Integer lado2;
+        private Integer lado3;
+
+    public Integer getLado1() {
+        return lado1;
+    }
+
+    public void setLado1(Integer lado1) {
+        this.lado1 = lado1;
+    }
+
+    public Integer getLado2() {
+        return lado2;
+    }
+
+    public void setLado2(Integer lado2) {
+        this.lado2 = lado2;
+    }
+
+    public Integer getLado3() {
+        return lado3;
+    }
+
+    public void setLado3(Integer lado3) {
+        this.lado3 = lado3;
+    }
+        
+         
 
     public float getArea() {
         return area;
@@ -17,15 +45,7 @@ public class Triangulo {
         this.area = area;
     }
 
-    public float getAltura() {
-        return altura;
-    }
-
-    public void setAltura(float altura) {
-        this.altura = altura;
-    }
-
-    public float getPerimetro() {
+       public float getPerimetro() {
         return perimetro;
     }
 
@@ -33,11 +53,11 @@ public class Triangulo {
         this.perimetro = perimetro;
        
     }
-    public float calcularlado (){
-        return 4*lado;
-        
-        
-    
+    public double calcularArea (){
+        double  p = (lado1 +lado2 +lado3) /2;
+        double area = p * (p- lado1) * (p - lado2) * (p - lado3);
+        return Math.sqrt(area);
+      
     }
     
 }
